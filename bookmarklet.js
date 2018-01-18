8546b22b034be0c2b94fd2dfd73a6dc07ed918dd
Masaya Kazama <miyan@t-p.jp>
2018-01-18 16:00:22 +0900

    Update copy complete message

--- bookmarklet.js
+++ bookmarklet.js
@@ -62,7 +62,7 @@
     tempForm.textContent = comments
     document.body.appendChild(tempForm)
     tempForm.select()
-    alert('copy' + (document.execCommand('copy') ? 'success' : 'failure'))
+    alert('copy: ' + (document.execCommand('copy') ? 'success' : 'failure'))
     document.body.removeChild(tempForm)
   }
 
